59dfbb8d13f362f7b3752a308fc226889c25c54e
unknown <okdiantoardi9@gmail.com>
2026-01-08 19:29:31 +0700

    model patment

---
+++ lib/models/payment/rinciansoa_model.dart
@@ -0,0 +1,30 @@
+import 'package:eassist_tools_app/models/payment/dngrandtotal_model.dart';
+import 'package:eassist_tools_app/models/payment/dnheadercob_model.dart';
+
+class RincianSOAModel {
+  final List<DnHeaderCobModel> headers;
+  final List<DnGrandTotalModel> grandtotal;
+
+  RincianSOAModel({
+    required this.headers,
+    required this.grandtotal,
+  });
+
+  factory RincianSOAModel.fromJson(Map<String, dynamic> json) {
+    return RincianSOAModel(
+      headers: (json['headers'] as List<dynamic>? ?? [])
+          .map((e) => DnHeaderCobModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      grandtotal: (json['grandtotal'] as List<dynamic>? ?? [])
+          .map((e) => DnGrandTotalModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'headers': headers.map((e) => e.toJson()).toList(),
+      'grandtotal': grandtotal.map((e) => e.toJson()).toList(),
+    };
+  }
+}
