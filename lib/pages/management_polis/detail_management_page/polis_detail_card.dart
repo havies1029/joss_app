03ef13feb1cb2f6ae669f0d7f1652057fd22ff74
Micky <dev.smartsoftid@gmail.com>
2026-03-12 09:01:59 +0700

    update otomatis dari git 2026-03-12 09:01am

--- lib/pages/management_polis/detail_management_page/polis_detail_card.dart
+++ lib/pages/management_polis/detail_management_page/polis_detail_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:joss_app/common/constants.dart';
-import 'package:path/path.dart';
 
 import '../../../blocs/regother/regother1crud_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
