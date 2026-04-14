387a44c9805deb8bea1aed921bdc75948bcb97f7
Ardi Okdianto <okdiantardi9@gmail.com>
2026-04-14 16:09:39 +0700

    t

--- lib/apis/combobox/combomkabzonagempa_api.dart
+++ lib/apis/combobox/combomkabzonagempa_api.dart
@@ -4,38 +4,22 @@ import 'package:http/http.dart' as http;
 import 'package:joss_app/models/combobox/combomkabzonagempa_model.dart';
 
 class ComboMKabZonaGempaAPI {
-	Future<List<ComboMKabZonaGempaModel>> getComboMKabZonaGempaAPI(
-			String wilayahId,
-			String searchText,
-			) async {
-		String urlGetComboEndPoint =
-				"${AppData.prefixEndPoint}/api/mkabzonagempacombobox/getlist";
 
-		final uri = Uri.parse(
-			"http://${AppData.httpAuthority}$urlGetComboEndPoint",
-		).replace(
-			queryParameters: {
-				"wilayahId": wilayahId,
-				"searchText": searchText,
-			},
-		);
-
-		final http.Response response = await http.get(
-			uri,
-			headers: <String, String>{
-				'Content-Type': 'application/json; odata=verbose',
-				'Accept': 'application/json; odata=verbose',
-				'Authorization': 'Bearer ${AppData.userToken}',
-			},
-		);
+	Future<List<ComboMKabZonaGempaModel>> getComboMKabZonaGempaAPI(String wilayahId, String searchText) async {
+		String urlGetComboEndPoint = "${AppData.prefixEndPoint}/api/mkabzonagempacombobox/getlist";
+    Map<String, String> queryParams = {"wilayahId": wilayahId, "searchText": searchText};
+		var uri = AppData.uriHtpp(AppData.httpAuthority, urlGetComboEndPoint, queryParams);
+		final http.Response response = await http.get(uri, headers: <String, String>{
+			'Content-Type': 'application/json; odata=verbos',
+			'Accept': 'application/json; odata=verbos',
+			'Authorization': 'Bearer ${AppData.userToken}'
+		});
 
 		if (response.statusCode == 200) {
 			final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
 			return parsed
-					.map<ComboMKabZonaGempaModel>(
-						(json) => ComboMKabZonaGempaModel.fromJson(json),
-			)
-					.toList();
+				.map<ComboMKabZonaGempaModel>((json) => ComboMKabZonaGempaModel.fromJson(json))
+				.toList();
 		} else {
 			throw Exception("Failed to load data");
 		}
